3583c8cbe9ea169e6718ac8f0b8fda5d0b93e611
nilsw13 <nils.wenting@laplateforme.io>
2025-01-28 12:48:47 +0100

    add api url

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',  // URL de l'API
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',  // URL de l'API
     headers: {
         'Content-Type': 'application/json',
     },
