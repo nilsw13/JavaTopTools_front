b9901633f3cd2b54d32c8ee437ebc33bee122a56
nilsw13 <nils.wenting@laplateforme.io>
2025-01-28 12:53:18 +0100

    fix bugs

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -2,8 +2,11 @@
 
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+console.log('API URL utilisée:', apiUrl); 
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',  // URL de l'API
+    baseURL: apiUrl || 'http://localhost:8080/api',  // URL de l'API
     headers: {
         'Content-Type': 'application/json',
     },
