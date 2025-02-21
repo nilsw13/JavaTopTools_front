966ef89a981479907313043a6fe133a0baba308b
nilsw13 <nils.wenting@laplateforme.io>
2025-02-21 20:46:43 +0100

    change footer link redirection

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <div>
         <div className="grid place-items-center h-[20%]">
-                <div className="flex flex-col justify-center items-center w-full mt-10 mb-10"> 
+                <div className="flex flex-col items-center justify-center w-full mt-10 mb-10"> 
                     <h1 className="text-3xl font-bold text-primary">Java Top Tools</h1>
-                    <p className="text-muted-foreground">Made with ❤️ by <a href="https://bento.me/nils-wenting" className="text-primary">NilsW13</a></p>
+                    <p className="text-muted-foreground">Made with ❤️ by <a href="https://linktr.ee/nilswenting" className="text-primary">NilsW13</a></p>
                 </div>
 
         </div>
